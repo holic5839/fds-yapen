c7f18d8713f3bb1e9fdf12f9fe8564565c8bae58
cheonmro <cheonmro@gmail.com>
2018-07-20 16:05:14 +0900

    Edit app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { YapenHeaderComponent } from './yapen-header.component';
@@ -22,7 +23,8 @@ import { YapenPayComponent } from './yapen-pay.component';
     YapenPayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
