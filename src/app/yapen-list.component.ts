aa9b173370d4f7ced5dafc2fefa1deb8deb2b6d3
Rootjang92 <janggeunho7@gmail.com>
2018-07-20 15:36:55 +0900

    change

--- src/app/yapen-list.component.ts
+++ src/app/yapen-list.component.ts
@@ -1,16 +1,20 @@
 // Jang geunho
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Room } from './yapen-container.component';
 
 @Component({
   selector: 'app-yapen-list',
   template: `
-    <p>
-      yapen-list works!
-    </p>
+   <ul *ngFor="let room of rooms">
+     <li> 이름 : {{ room.roomName }} </li>
+     <li> 가격 : {{ room.roomFee }} </li>
+     <li> 지역 : {{ room.roomRegion }} </li>
+   </ul>
   `,
   styles: []
 })
 export class YapenListComponent implements OnInit {
+  @Input() rooms: Room[];
 
   constructor() { }
 
