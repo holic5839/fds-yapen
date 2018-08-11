bddd4d2d1a99eac87fbe8b95830eb0118b953436
holic5839 <holic5839@gmail.com>
2018-08-11 15:51:25 +0900

    Revert "Develop"

--- src/app/yapen-header.component.ts
+++ src/app/yapen-header.component.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { AppRouterModule } from './yapen-route';
 
 @Component({
   selector: 'app-yapen-header',
   template: `
-
-  <input type="button" value="home" onclick="self.location='http://localhost:4200/#/main'">
-
+    <p>
+      yapen-header works!
+    </p>
   `,
   styles: []
 })
 export class YapenHeaderComponent implements OnInit {
 
-
-  constructor() {}
+  constructor() { }
 
   ngOnInit() {
   }
--- src/app/yapen-reserve.component.ts
+++ src/app/yapen-reserve.component.ts
@@ -1,66 +1,20 @@
 // 노천명
-import { Component, OnInit, Input } from '@angular/core';
-import { Room } from './yapen-container.component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-yapen-reserve',
   template: `
-    <section class="reserve">
-      <table class="reserve-table">
-        <tr>
-          <td>이름</td>
-          <td>애월펜션</td>
-        </tr>
-        <tr>
-          <td>가격</td>
-          <td>256,000<span>원</span></td>
-        </tr>
-        <tr>
-          <td>지역</td>
-          <td>제주도</td>
-        </tr>
-      </table>
-      <button type="submit" class="btn-reserve" (click)="moveToPayPage()">예약하기</button>
-    </section>
-
+    <p>
+      yapen-reserve works!
+    </p>
   `,
-  styles: [`
-    .reserve{
-      position: absolute;
-      top: 30%;
-      left: 40%;
-    }
-    .reserve-table{
-      border: 1px solid #aaa;
-      background-color: #ccc;
-      width: 300px;
-    }
-    td{
-      border: 1px solid #aaa;
-    }
-    .btn-reserve{
-      background-color: green;
-      background: #ff6559;
-      color: #fff;
-      padding: 10px 100px;
-      font-weight: bold;
-      font-size: 14px;
-      border-radius: 3px;
-      margin-top: 10px;
-      margin-left: 20px;
-    }
-  `]
+  styles: []
 })
 export class YapenReserveComponent implements OnInit {
-  @Input() rooms: Room[];
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  moveToPayPage() {
-    location.href = 'http://localhost:4200/#/pay';
   }
 
 }
